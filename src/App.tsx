4fb5427ed9d0c7e0a01af3995e1704ca9a4f8774
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-02-16 23:11:34 +0530

    Create navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {NavigationBar} from "./components/NavigationBar";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavigationBar/>
+        </div>
+    );
 }
 
 export default App;
