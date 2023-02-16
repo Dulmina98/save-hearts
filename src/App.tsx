e7b2bc834ef6835fcff0b39e980657c004cb0e71
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-02-17 00:33:16 +0530

    Add footer to App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {NavigationBar} from "./components/NavigationBar";
 import {ReserveTimePage} from "./pages/ReserveTimePage";
+import {Footer} from "./components/Footer";
 
 function App() {
     return (
         <div className="App">
             <NavigationBar/>
             <ReserveTimePage/>
+            <Footer/>
         </div>
     );
 }
