b7089a71d616f0df0d8562b66b0b0d6d50cf448d
savindu29 <1savindupanagoda@gmail.com>
2023-02-20 14:59:33 +0530

    added ML Modules

--- src/pages/OrganizeCampPage.tsx
+++ src/pages/OrganizeCampPage.tsx
@@ -35,6 +35,7 @@ export function OrganizeCampPage() {
         {value: 24, label: 'Batticaloa'},
         {value: 25, label: 'Ampara'},
     ];
+    const n = 10;
 
     const date = [
         {value: 1, label: '02nd March 2023'},
