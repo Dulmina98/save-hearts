0b97a7ac6cc0fa2c08d5b42df145ea178053578d
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-03-26 01:33:52 +0530

    Update react routers

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import React from 'react';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {HomePage} from "./pages/HomePage";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {NavigationBar} from "./components/NavigationBar";
+import {OrganizeCampPage} from "./pages/OrganizeCampPage";
+import {ReserveTimePage} from "./pages/ReserveTimePage";
+import {AdminDashboardPage} from "./pages/AdminDashboardPage";
 
 function App() {
     return (
         <div className="App">
-            <HomePage/>
+            <NavigationBar/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path={'/'} element={<HomePage/>}/>
+                    <Route path={'/organize-camp'} element={<OrganizeCampPage/>}/>
+                    <Route path={'/reserve-time'} element={<ReserveTimePage/>}/>
+                    <Route path={'/admin-dashboard'} element={<AdminDashboardPage/>}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
