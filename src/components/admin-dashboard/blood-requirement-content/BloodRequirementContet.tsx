e33ab613a9e591a21f8a4fd3d030cc7d8463e2dc
Yasith Lankesha <yasith.lankezz@gmail.com>
2023-04-06 10:02:49 +0530

    updated graph

--- src/components/admin-dashboard/blood-requirement-content/BloodRequirementContet.tsx
+++ src/components/admin-dashboard/blood-requirement-content/BloodRequirementContet.tsx
@@ -48,7 +48,15 @@ export function BloodRequirementContet() {
         {"January": 0},
         {"February": 0},
         {"March": 0},
-        {"April": 0}
+        {"April": 0},
+        {"May": 0},
+        {"June": 0},
+        {"July": 0},
+        {"August": 0},
+        {"September": 0},
+        {"October": 0},
+        {"November": 0},
+        {"December": 0}
     ]);
     useEffect(() => {
         async function fetchData() {
